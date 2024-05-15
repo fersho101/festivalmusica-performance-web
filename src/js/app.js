d16b67b12da83c00cb9d41bd406b2aa5f2376b6c
fersho101 <corral.fdo@gmail.com>
2024-05-14 18:59:19 -0700

    sirviendo imgs avif y webp

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     navegacionFija()
     crearGaleria()
     resaltarEnlace()
@@ -9,8 +9,8 @@ function navegacionFija() {
     const header = document.querySelector('.header')
     const sobreFestival = document.querySelector('.sobre-festival')
 
-    document.addEventListener('scroll', function() {
-        if(sobreFestival.getBoundingClientRect().bottom < 1) {
+    document.addEventListener('scroll', function () {
+        if (sobreFestival.getBoundingClientRect().bottom < 1) {
             header.classList.add('fixed')
         } else {
             header.classList.remove('fixed')
@@ -23,26 +23,31 @@ function crearGaleria() {
     const CANTIDAD_IMAGENES = 16
     const galeria = document.querySelector('.galeria-imagenes')
 
-    for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.loading = 'lazy'
-        imagen.width = '300'
-        imagen.height = '300'
-        imagen.src = `src/img/gallery/thumb/${i}.jpg`
-        imagen.alt = 'Imagen Galería'
+    for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
+
+
         // Event Handler
-        imagen.onclick = function() {
+        imagen.onclick = function () {
             mostrarImagen(i)
         }
-        
+
         galeria.appendChild(imagen)
     }
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = 'Imagen Galería'
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+`;
 
     // Generar Modal
     const modal = document.createElement('DIV')
@@ -79,22 +84,22 @@ function cerrarModal() {
 }
 
 function resaltarEnlace() {
-    document.addEventListener('scroll', function() {
+    document.addEventListener('scroll', function () {
         const sections = document.querySelectorAll('section')
         const navLinks = document.querySelectorAll('.navegacion-principal a')
 
         let actual = '';
-        sections.forEach( section => {
+        sections.forEach(section => {
             const sectionTop = section.offsetTop
             const sectionHeight = section.clientHeight
-            if(window.scrollY >= (sectionTop - sectionHeight / 3 ) ) {
+            if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
                 actual = section.id
             }
         })
 
         navLinks.forEach(link => {
             link.classList.remove('active')
-            if(link.getAttribute('href') === '#' + actual) {
+            if (link.getAttribute('href') === '#' + actual) {
                 link.classList.add('active')
             }
         })
@@ -104,13 +109,13 @@ function resaltarEnlace() {
 function scrollNav() {
     const navLinks = document.querySelectorAll('.navegacion-principal a')
 
-    navLinks.forEach( link => {
+    navLinks.forEach(link => {
         link.addEventListener('click', e => {
             e.preventDefault()
             const sectionScroll = e.target.getAttribute('href')
             const section = document.querySelector(sectionScroll)
 
-            section.scrollIntoView({behavior: 'smooth'})
+            section.scrollIntoView({ behavior: 'smooth' })
         })
     })
 }
